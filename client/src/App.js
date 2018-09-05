bd72f802a0af5486b4f19a90bef3e22c3a555554
Josh <jobr3255@colorado.edu>
2018-09-05 13:35:26 -0600

    Everything seems to be working...

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,48 @@
 import React, { Component } from 'react';
-
-import logo from './logo.svg';
-
 import './App.css';
 
 class App extends Component {
+  // Initialize state
   state = {
-    response: null
-  };
+    passwords: [],
+    something: null,
+    response: null }
 
+  // Fetch passwords after first mount
   componentDidMount() {
-    // this.setState({ response: res.data })
-    // this.setState({ response: res })
-    this.callApi()
-      .then(res => console.log(res) )
+    this.getPasswords();
+  }
+
+  getPasswords = () => {
+    // Get the passwords and store them in state
+    fetch('/api/passwords')
+      .then(res => res.json())
+      .catch(err => console.log(err))
+      .then(passwords => this.setState({ passwords }));
+  }
+
+  getSomething = () => {
+    // Get the passwords and store them in state
+    fetch('/api/something')
+      .then(res => res.json())
+      .then(something => this.setState({ something: something.data }));
+  }
+
+  getUsers = () => {
+    // Get the passwords and store them in state
+    fetch('/api/users')
+      .then(res => res.json())
+      .then(res => this.setState({ response: res }) )
       .catch(err => console.log(err));
   }
 
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-    //console.log(body);
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
   render() {
-    let content;
-    if(this.state.response != null){
+    const { passwords, something, response } = this.state;
+    let content = <div>No users</div>;;
+    if(response != null){
       content = (
         <div className="App-intro">
-          {this.state.response.map(
+          {response.map(
                 (user) =>
               <div key={user.id}>
                 {user.id} {user.name}
@@ -42,11 +53,59 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        {content}
+        <div>
+          {something}
+          <br/>
+          <button
+            className="more"
+            onClick={this.getSomething}>
+            Get Something
+          </button>
+        </div>
+        {/* Render the passwords if we have them */}
+        {passwords.length ? (
+          <div>
+            <h1>5 Passwords.</h1>
+            <ul className="passwords">
+              {/*
+                Generally it's bad to use "index" as a key.
+                It's ok for this example because there will always
+                be the same number of passwords, and they never
+                change positions in the array.
+              */}
+              {passwords.map((password, index) =>
+                <li key={index}>
+                  {password}
+                </li>
+              )}
+            </ul>
+            <button
+              className="more"
+              onClick={this.getPasswords}>
+              Get More
+            </button>
+          </div>
+        ) : (
+          // Render a helpful message otherwise
+          <div>
+            <h1>No Passwords</h1>
+            <button
+              className="more"
+              onClick={this.getPasswords}>
+              Try Again?
+            </button>
+          </div>
+        )}
+        <div>
+          <div>
+            {content}
+          </div>
+          <button
+            className="more"
+            onClick={this.getUsers}>
+            Get Users
+          </button>
+        </div>
       </div>
     );
   }
---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const path = require('path');
+const generatePassword = require('password-generator');
+
+const app = express();
+
+var mysql      = require('mysql');
+
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'test'
+});
+connection.connect();
+app.get('/api/users', (req, res) => {
+	//connection.connect();
+	connection.query('SELECT * from users', function(err, rows, fields) {
+		if (!err) {
+      //res.send(rows);
+      //var toSend = JSON.stringify( { data: [{ id: 1, name: 'Josh' }, {id: 2, name: Jeremy }] });
+      //console.log(toSend);
+
+      toSend = {
+        data: JSON.stringify(rows)
+      };
+      res.send(rows);
+      //res.send({ data: 'Hello From Express' });
+		} else {
+			console.log('Error while performing Query.');
+		}
+	});
+});
+
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Put all API endpoints under '/api'
+app.get('/api/passwords', (req, res) => {
+  const count = 5;
+
+  // Generate some passwords
+  const passwords = Array.from(Array(count).keys()).map(i =>
+    generatePassword(12, false)
+  )
+
+  // Return them as json
+  res.json(passwords);
+
+  console.log(`Sent ${count} passwords`);
+});
+
+app.get('/api/something', (req, res) => {
+  res.send({ data: 'SOMETHING' });
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Password generator listening on ${port}`);
